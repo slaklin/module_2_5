e737b761f5e577ccb21a25cbf9fa472d30ecdd49
slaklin <120272257+slaklin@users.noreply.github.com>
2024-09-07 08:26:00 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,13 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+
+created_matrix = get_matrix(4, 2, 13)
+print(created_matrix)
+
+
